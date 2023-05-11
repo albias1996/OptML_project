36237cd7012eb9adb7dc88d9dfff9a5be186004d
albias1996 <115004258+albias1996@users.noreply.github.com>
2023-05-11 23:34:18 +0200

    add SGD parameters

--- params.py
+++ params.py
@@ -1,9 +1,16 @@
 
-## Useful parameters for ADAM optimizer
-
-
+## parameters for ADAM optimizer
 
 ADAM_LEARNING_RATE = 0.001
 ADAM_BATCH_SIZE = 32
 ADAM_N_EPOCHS = 15
+
+
+## parameters for SGD optimizer 
+
+SGD_LEARNING_RATE = 0.01
+SGD_BATCH_SIZE = 128
+SGD_N_EPOCHS = 10 
+
+
 

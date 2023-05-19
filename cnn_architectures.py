2134b65c562669d8eb95477ee9abae4eedd26fa4
albias1996 <115004258+albias1996@users.noreply.github.com>
2023-05-19 14:45:40 +0200

    Add files via upload

--- cnn_architectures.py
+++ cnn_architectures.py
@@ -46,54 +46,8 @@ class LeNet5(nn.Module):
     
 
 
-class AlexNet(nn.Module):
-
-    #need to resize data from 28x28 to 227x227 in order to use AlexNet
-
-    def __init__(self, num_classes):
-        super(AlexNet, self).__init__()
-
-        self.conv1 = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=96, kernel_size=11, stride=4, padding=0),
-                                   nn.ReLU(),
-                                   nn.MaxPool2d(kernel_size=3, stride=2))
-        
-        self.conv2 = nn.Sequential(nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2),
-                                   nn.ReLU(),
-                                   nn.MaxPool2d(kernel_size=3, stride=2))
-        
-        self.conv3 = nn.Sequential(nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
-                                   nn.ReLU())
-        
-        self.conv4 = nn.Sequential(nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1),
-                                   nn.ReLU())
-        
-        self.conv5 = nn.Sequential(nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
-                                   nn.ReLU(),
-                                   nn.MaxPool2d(kernel_size=3, stride=2))
-        
-        self.fc1 = nn.Linear(in_features=256*6*6, out_features=4096)
-        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
-        self.fc3 = nn.Linear(in_features=4096, out_features=num_classes)
 
 
-    def forward(self, x):
-        #passing trhough convulation layers and flatten output tensor
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = x.view(-1, x.size(0))
-
-        #passing through linear layers
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, 0.5)
-        x = F.relu(self.fc2(x))
-        x = F.dropout(x, 0.5)
-        logits = self.fc3(x)
-        probs = F.log_softmax(logits, dim=1)
-
-        return probs
 
 
 

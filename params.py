a900e03f47cbe172bd1e8b9af85fd0e1c0f505db
mossinel <giacomo.mossinelli@epfl.ch>
2023-06-03 17:19:05 +0200

    second order hyperparams

--- params.py
+++ params.py
@@ -28,13 +28,13 @@ ADAGRAD_N_EPOCHS = 15
 
 ## Parameters for ADAHessian (AH) optimizer 
 
-AH_LEARNING_RATE = 0.01
+AH_LEARNING_RATE = 0.1
 AH_BATCH_SIZE = 128
 AH_N_EPOCHS = 10 
-AH_BETAS = ...
-AH_EPS = ...
-AH_WD = ...
-AH_POWER = ...
+AH_BETAS = [0.1, 0.1]
+AH_EPS = 0.0001
+AH_WD = 0.0001
+AH_POWER = 0.5
 
 
 

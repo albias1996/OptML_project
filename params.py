8442c6430e589c6a83de2ab8dd2f56f37c22c277
albias1996 <albias.havolli@epfl.ch>
2023-06-11 19:45:41 +0200

    update parameters

--- params.py
+++ params.py
@@ -3,34 +3,34 @@
 # in terms of accuracy
 
 
-## Parameters for ADAM optimizer (good performance)
+## Parameters for ADAM optimizer 
 
 ADAM_LEARNING_RATE = 0.001
-ADAM_BATCH_SIZE = 32
-ADAM_N_EPOCHS = 15
+ADAM_BATCH_SIZE = 100
+ADAM_N_EPOCHS = 150
 
 
-## Parameters for SGD optimizer (good performance)
+## Parameters for SGD optimizer 
 
-SGD_LEARNING_RATE = 0.01
-SGD_BATCH_SIZE = 128
-SGD_N_EPOCHS = 20
+SGD_LEARNING_RATE = 0.1
+SGD_BATCH_SIZE = 100
+SGD_N_EPOCHS = 150
 
 
-## Parameters for AdaGrad optimizer (good performance)
+## Parameters for AdaGrad optimizer 
 
 ADAGRAD_LEARNING_RATE = 0.01
 ADAGRAD_DECAY = 0
 ADAGRAD_INITIAL_ACCUMULATOR_VALUE = 0
 ADAGRAD_BATCH_SIZE = 100
-ADAGRAD_N_EPOCHS = 15
+ADAGRAD_N_EPOCHS = 150
 
 
 ## Parameters for ADAHessian (AH) optimizer 
 
 AH_LEARNING_RATE = 0.1
-AH_BATCH_SIZE = 128
-AH_N_EPOCHS = 10 
+AH_BATCH_SIZE = 100
+AH_N_EPOCHS = 150 
 AH_BETAS = [0.1, 0.1]
 AH_EPS = 0.0001
 AH_WD = 0.0001
